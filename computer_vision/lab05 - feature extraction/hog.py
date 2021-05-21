90a6cb39a9327a6320d6886957c453199963ad1c
materight <matteo.est@gmail.com>
2021-05-21 13:10:34 +0200

    Added SIFT lab

--- computer_vision/lab05 - feature extraction/hog.py
+++ computer_vision/lab05 - feature extraction/hog.py
@@ -1,5 +1,6 @@
 import numpy as np
 import cv2 as cv
+from skimage import exposure, feature
 
 cap = cv.VideoCapture('../material/Video.mp4')
 
@@ -16,15 +17,21 @@ hog.setSVMDetector(cv.HOGDescriptor_getDefaultPeopleDetector())
 while cap.isOpened():
     ret, frame = cap.read()
 
-    # COmpute bounding boxes
+    # Compute bounding boxes
     rects, weights = hog.detectMultiScale(frame)
-
-    # Plot results
+    
+    # Recompute HOG features for visualization
+    H, hogImage = feature.hog(frame, orientations=9, pixels_per_cell=(16,16), cells_per_block=(2,2), transform_sqrt=True, block_norm='L1', visualize=True)
+    hogImage = exposure.rescale_intensity(hogImage, out_range=(50, 255)).astype(np.uint8) # Rescale intensity to make features more visible
+    
+    # Plot rectangular bounding boxes
     for rect in rects:
         # rect = x, y, height, width
         cv.rectangle(frame, (rect[0], rect[1]), (rect[0]+rect[2], rect[1]+rect[3]), (0, 0, 255), 2)
-    cv.imshow('HOG', frame)
     
+    cv.imshow('HOG features', hogImage)
+    cv.imshow('Detected people', frame)
+
     if cv.waitKey(1) & 0xFF == ord('q'):
         break
 
---
+++ computer_vision/lab05 - feature extraction/sift.py
@@ -0,0 +1,65 @@
+from typing import Match
+import numpy as np
+import cv2 as cv
+
+# Read images in grayscale
+img_obj = cv.imread('../material/img/book.png', 0) # "book.png" or "box.png"
+img_scene = cv.imread('../material/img/box_in_scene.png', 0)
+
+img_obj2 = img_obj.copy()
+img_scene2 = img_scene.copy()
+
+'''
+Step 1: apply SIFT
+'''
+# Detect features on the image: compute keypoints and their descriptors
+sift = cv.SIFT_create(400)
+kp_obj, dsc_obj = sift.detectAndCompute(img_obj, None) 
+kp_scene, dsc_scene = sift.detectAndCompute(img_scene, None) 
+
+# Plot extracted keypoints
+img_obj = cv.drawKeypoints(img_obj, kp_obj, dsc_obj, flags=cv.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+img_scene = cv.drawKeypoints(img_scene, kp_scene, dsc_scene, flags=cv.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+
+'''
+Step 2: compute matching
+'''
+# Match descriptors between images
+bf = cv.BFMatcher(cv.NORM_L2)
+matches = bf.match(dsc_obj, dsc_scene)
+
+# Filter out "bad" matches
+matches = list(filter(lambda x : x.distance < 150, matches))
+
+# Plot computed matches
+img_matches = cv.drawMatches(img_obj, kp_obj, img_scene, kp_scene, matches, None, flags=cv.DRAW_MATCHES_FLAGS_NOT_DRAW_SINGLE_POINTS)
+
+'''
+Step 2: compute stitching
+'''
+# Get points of good matches and transform to array of tuples
+pts_obj = np.float32([ kp_obj[m.queryIdx].pt for m in matches ]).reshape(-1, 1, 2)
+pts_scene = np.float32([ kp_scene[m.trainIdx].pt for m in matches ]).reshape(-1, 1, 2)
+
+# Compute homography
+H, masked = cv.findHomography(pts_obj, pts_scene, cv.RANSAC) # RANSAC: uses the matches that are more parrallel as possible w.r.t. each other 
+print('Homography matrix:\n', H)
+
+# Warp object image using the computed homography
+warped_obj = cv.warpPerspective(img_obj2, H, (img_scene2.shape[1], img_scene2.shape[0]))
+
+# Apply threshold to get black colors
+warped_mask = cv.inRange(warped_obj, 0, 1)
+
+# Stitch image using the warped mask
+img_stitch = img_scene2.copy()
+img_stitch[warped_mask == 0] = warped_obj[warped_mask == 0]
+
+# Plot results
+cv.imshow('Object SIFT keypoints', img_obj)
+cv.imshow('Scene SIFT keypoints', img_scene)
+cv.imshow('Keypoints matches', img_matches)
+cv.imshow('Warped object', warped_obj)
+cv.imshow('Warped object mask', warped_mask)
+cv.imshow('Stitching result', img_stitch)
+cv.waitKey(0)
